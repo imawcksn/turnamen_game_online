4e61c6be3a20d488a7f0ea37bd559d5d04176a3d
Imam Wicaksono Suherman <imamwicaksonosuherman@gmail.com>
2024-11-17 11:11:43 +0700

    add logout and check condition register

--- public/functions.php
+++ public/functions.php
@@ -4,9 +4,26 @@ include('../config/db.php');
 function register($usn, $pw)
 {
     global $conn;
-    $pwHash = password_hash($pw, PASSWORD_DEFAULT);
-    $sql = "INSERT INTO users (username, password, role) VALUES ('$usn', '$pwHash', 'user')";
-    return $conn->query($sql);
+
+    // Check if the username already exists
+    $sqlCheck = "SELECT * FROM users WHERE username = '$usn'";
+    $result = $conn->query($sqlCheck);
+
+    if ($result->num_rows > 0) {
+        // Username already exists
+        return false;
+    } else {
+        // Hash the password and insert new user
+        $pwHash = password_hash($pw, PASSWORD_DEFAULT);
+        $sqlInsert = "INSERT INTO users (username, password, role) VALUES ('$usn', '$pwHash', 'user')";
+        
+        // Execute insert query
+        if ($conn->query($sqlInsert)) {
+            return true;
+        } else {
+            return false; // If there is an issue with the query
+        }
+    }
 }
 
 function login($usn, $pw)
@@ -22,6 +39,20 @@ function login($usn, $pw)
         }
     }
     return false;
+}
+
+function logout() {
+    // Start the session
+    session_start();
+
+    // Unset all session variables
+    session_unset();
+
+    // Destroy the session
+    session_destroy();
+    
+    header('Location: index.php');
+    exit();
 }
 
 function create_tournament($name, $description, $schedule, $livestream)
