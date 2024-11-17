813527f37b4c774dcbffb2168c478a8caeb95e07
Imam Wicaksono Suherman <imamwicaksonosuherman@gmail.com>
2024-11-17 11:09:49 +0700

    add footer contact

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,7 @@
 
 <footer class="bg-dark text-white text-center p-4 mt-4">
         <p>&copy; OGT2K24.</p>
+        <p>Ingin mendaftarkan turnamen anda? Kontak kami.</p>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
