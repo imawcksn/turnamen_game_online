6ce35ca1b3539a08bae78e3c50465537e4b57650
Imam Wicaksono Suherman <imamwicaksonosuherman@gmail.com>
2024-11-24 08:32:16 +0700

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,26 @@
+<style>html, body {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+}
 
-<footer class="bg-dark text-white text-center p-4 mt-4">
-        <p>&copy; OGT2K24.</p>
-        <p>Ingin mendaftarkan turnamen anda? Kontak kami.</p>
-    </footer>
+body {
+    display: flex;
+    flex-direction: column;
+}
+
+footer {
+    color: #151314;
+    margin-top: auto;
+}
+</style>
+
+
+<footer class="footer text-white text-center p-4 mt-auto">
+    <p>&copy; OGT2K24.</p>
+    <p>Ingin mendaftarkan turnamen anda? Kontak kami.</p>
+</footer>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
