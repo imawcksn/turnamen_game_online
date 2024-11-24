22def33bb512d23e0ba3bee3567f861610397dcf
Imam Wicaksono Suherman <imamwicaksonosuherman@gmail.com>
2024-11-24 11:49:52 +0700

    add google form to kontak kami

--- includes/footer.php
+++ includes/footer.php
@@ -18,7 +18,7 @@ footer {
 
 <footer class="footer text-white text-center p-4 mt-auto">
     <p>&copy; OGT2K24.</p>
-    <p>Ingin mendaftarkan turnamen anda? Kontak kami.</p>
+    <p>Ingin mendaftarkan turnamen anda? <a href="https://forms.gle/oBC55Z6TynWJ1D6c7">Kontak kami.</a></p>
 </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
